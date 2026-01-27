98a25416cdcfc00608e8eff98dbaa7162b8cc7ce
RicardoC <ricardo.cacheira@hotmail.com>
2026-01-27 15:21:53 +0000

    1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,31 +1,52 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
+const Statistics = (props) => {
+  if (props.reviews.good == 0 && props.reviews.neutral == 0 && props.reviews.bad == 0) {
+    return (
+      <div>
+        No reviews given
+      </div>
+    )
+  }
+  return (
+    <div>
+      good {props.reviews.good} <br />
+      neutral {props.reviews.neutral} <br />
+      bad {props.reviews.bad} <br />
+    </div>
+  )
+}
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
+const Title = ({ text }) => <h1>{text}</h1>
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />   
+      <Title text='give feedback' />
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      <Title text='statistics' />
+      <Statistics reviews={{ good, neutral, bad }} />
     </div>
   )
 }
