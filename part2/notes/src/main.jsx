a17a0c7a1dc42fd295527d5ce7f8a0d0810bf26c
RicardoC <ricardo.cacheira@hotmail.com>
2026-01-30 11:37:49 +0000

    use effect example

--- part2/notes/src/main.jsx
+++ part2/notes/src/main.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 import App from './App'
+
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
 
 const notes = [
   {
@@ -19,6 +25,6 @@ const notes = [
   }
 ]
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <App notes={notes} />
+// )
