df595bdfaadf8fcda9e9d946d1a66eaccdc2282c
RicardoC <ricardo.cacheira@hotmail.com>
2026-02-04 14:52:16 +0000

    Middleware

--- part3/persons/index.js
+++ part3/persons/index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
 let people =
 [
     { 
@@ -90,6 +100,12 @@ app.post('/api/persons', (request, response) => {
     people = people.concat(person)
     response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
