7cae26c3722e313bbdd0965df75bdd3a4c702d16
RicardoCacheira <ricardo.cacheira@hotmail.com>
2026-02-09 11:58:09 +0000

    3.20

--- part3/persons/models/phone.js
+++ part3/persons/models/phone.js
@@ -20,7 +20,17 @@ const numberSchema = new mongoose.Schema({
       minLength: 3,
       required: true
     },
-    number: String,
+    number: {
+      type: String,
+      validate:
+      {
+        validator: function(v) {
+          return /\d{2,3}-\d{7,8}/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+      required: true
+    }
 })
 
 numberSchema.set('toJSON', {
