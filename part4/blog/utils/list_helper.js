1808aab58cff3a34e0ae9342a8b1b52af206f906
RicardoCacheira <ricardo.cacheira@hotmail.com>
2026-02-10 15:43:35 +0000

    4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,19 @@ const likeCount = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const favorite = blogs.reduce((prev, current) => {
+    return (prev.likes > current.likes) ? prev : current
+  })
+  return favorite
+}
+
 module.exports = {
   dummy,
-  likeCount
+  likeCount,
+  favoriteBlog
 }
