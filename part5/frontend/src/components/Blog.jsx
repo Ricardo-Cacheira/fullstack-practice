1ce7137420eb35687699fdc6e1be0f24da5d22bd
RicardoCacheira <ricardo.cacheira@hotmail.com>
2026-02-18 12:03:00 +0000

    5.7
    Expanding blog

--- part5/frontend/src/components/Blog.jsx
+++ part5/frontend/src/components/Blog.jsx
@@ -1,7 +1,54 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const minimizedBlog = () =>
+  (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? 'hide': 'view'}
+        </button>
+      </div>
+    </div>
+  )
+
+  const fullBlog = () =>
+  (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? 'hide': 'view'}
+        </button>
+      </div>
+      <li>{blog.url}</li>
+      <li>
+        likes {blog.likes}
+        <button onClick={() => {}}>
+          like
+        </button>
+      </li>
+      <li>{blog.user.name}</li>
+    </div>
+  )
+
+  return (
+    <div>
+      {!visible && minimizedBlog()}
+      {visible && fullBlog()}
+    </div>
+  )
+}
 
 export default Blog
