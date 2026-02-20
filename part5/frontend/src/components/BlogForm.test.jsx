0b5304ad23a66791d2c495d145d18923244b813c
RicardoCacheira <ricardo.cacheira@hotmail.com>
2026-02-20 12:41:04 +0000

    5.16
    BlogForm test

---
+++ part5/frontend/src/components/BlogForm.test.jsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import BlogForm from './BlogForm'
+
+describe('<Blog />', () => {
+
+  const mockHandler = vi.fn()
+  const blog = {
+    title: 'Blog title',
+    author: 'Author name',
+    url: 'exampleUrl',
+    likes: 0,
+    user: {
+      username: 'testUser',
+      name: 'Test User'
+    }
+  }
+  beforeEach(() => {
+
+  })
+
+  test('<BlogForm /> updates parent state and calls onSubmit', async () => {
+    const createBlog = vi.fn()
+    const user = userEvent.setup()
+    render(
+      <BlogForm
+        createBlog={createBlog}
+      />
+    )
+
+    const titleInput = screen.getByLabelText('title:')
+    const authorInput = screen.getByLabelText('author:')
+    const urlInput = screen.getByLabelText('url:')
+    const sendButton = screen.getByText('create')
+
+    await user.type(titleInput, 'Test Title')
+    await user.type(authorInput, 'Author Name')
+    await user.type(urlInput, 'testUrl.com')
+    await user.click(sendButton)
+    console.log(createBlog.mock.calls)
+
+    expect(createBlog.mock.calls).toHaveLength(1)
+    expect(createBlog.mock.calls[0][0].title).toBe('Test Title')
+    expect(createBlog.mock.calls[0][0].author).toBe('Author Name')
+    expect(createBlog.mock.calls[0][0].url).toBe('testUrl.com')
+  })
+})
