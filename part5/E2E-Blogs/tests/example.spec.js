81580e840bba8c5e63b523a2e0c32683d06c850d
RicardoCacheira <ricardo.cacheira@hotmail.com>
2026-02-21 12:33:21 +0000

    5.18
    Testing login

--- part5/E2E-Blogs/tests/example.spec.js
+++ part5/E2E-Blogs/tests/example.spec.js
@@ -1,12 +1,41 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { createBlog, loginWith } = require('./helper')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Blog Tester',
+        username: 'test',
+        password: 'password123'
+      }
+    })
+
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
     const locator = page.getByText('username')
     await expect(locator).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('login fails with wrong password', async ({ page }) => {
+      await loginWith(page, 'test', 'wrong')
+
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('wrong username or password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+
+      await expect(page.getByText('Blog Tester logged in')).not.toBeVisible()
+    })
+
+    test('user can log in', async ({ page }) => {
+      await loginWith(page, 'test', 'password123')
+
+      await expect(page.getByText('Blog Tester logged in')).toBeVisible()
+    })
   })
 })
---
+++ part5/E2E-Blogs/tests/helper.js
@@ -0,0 +1,15 @@
+const loginWith = async (page, username, password)  => {
+  await page.getByRole('button', { name: 'login' }).click()
+  await page.getByLabel('username').fill(username)
+  await page.getByLabel('password').fill(password)
+  await page.getByRole('button', { name: 'login' }).click()
+}
+
+const createBlog = async (page, content) => {
+  await page.getByRole('button', { name: 'new Blog' }).click()
+  await page.getByRole('textbox').fill(content)
+  await page.getByRole('button', { name: 'save' }).click()
+  await page.getByText(content).waitFor()
+}
+
+export { loginWith, createBlog }
