d8c2dc6410e4a009334d3ccf06c909b7573e123b
RicardoCacheira <ricardo.cacheira@hotmail.com>
2026-02-23 11:59:45 +0000

    Presentational and Container components

--- part6/notes/src/main.jsx
+++ part6/notes/src/main.jsx
@@ -7,6 +7,25 @@ import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
 
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
